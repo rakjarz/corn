56e37d8ad2481c43455e4eb0db28f63021205b30
Vu Phan <ptvu.it@gmail.com>
2023-08-11 02:11:51 -0700

    feat: auto increase build number

--- corn/build.gradle.kts
+++ corn/build.gradle.kts
@@ -1,6 +1,9 @@
+import java.util.Properties
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -36,4 +39,66 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.10.1")
     testImplementation("junit:junit:4.13.2")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                pom {
+                    name.set("Corn")
+                    description.set("Kotlin logging file for android")
+                    url.set("https://github.com/rakjarz/corn")
+
+                    developers {
+                        developer {
+                            id.set("vuptt")
+                            name.set("Vu Phan")
+                            email.set("ptvu.it@gmail.com")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:git://github.com/rakjarz/corn.git")
+                        developerConnection.set("scm:git:git://github.com/rakjarz/corn.git")
+                        url.set("https://github.com/rakjarz/corn")
+                    }
+                }
+
+                groupId = "net.rakjarz"
+                artifactId = "corn"
+                version = computeVersionName()
+
+                from(components["release"])
+            }
+        }
+    }
+}
+
+fun computeVersionName(): String {
+    val runTasks = gradle.startParameter.taskNames
+    val versionPropsFile = file("version.properties")
+
+    if (versionPropsFile.canRead()) {
+        val versionProps = Properties()
+        versionProps.load(versionPropsFile.reader())
+        var value = 0
+
+        if ("internalRelease" in runTasks || "iR" in runTasks) {
+            value = 1
+        }
+
+        val versionMajor = 1
+        val versionMinor = 0
+        val versionPatch = versionProps.getProperty("VERSION_PATCH").toInt() + value
+        val versionBuild = versionProps.getProperty("VERSION_BUILD").toInt() + 1
+
+        versionProps.setProperty("VERSION_PATCH", versionPatch.toString())
+        versionProps.setProperty("VERSION_BUILD", versionBuild.toString())
+        versionProps.setProperty("version", "$versionMajor.$versionMinor.$versionPatch")
+        versionProps.store(versionPropsFile.writer(), null)
+        return "$versionMajor.$versionMinor.$versionPatch"
+    } else {
+        throw GradleException("Could not read version.properties!")
+    }
 }
