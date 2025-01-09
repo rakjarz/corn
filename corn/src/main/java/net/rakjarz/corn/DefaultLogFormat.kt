9f4ead473705fe223f486f1ed9bc92043748fac6
Vu Phan <ptvu.it@gmail.com>
2025-01-09 22:41:48 +0700

    chore: use full log level

--- corn/src/main/java/net/rakjarz/corn/DefaultLogFormat.kt
+++ corn/src/main/java/net/rakjarz/corn/DefaultLogFormat.kt
@@ -8,6 +8,6 @@ class DefaultLogFormat : LogFormat {
     private val logLineDataFormat = SimpleDateFormat("MMM-dd HH:mm:ss", Locale.US)
 
     override fun format(log: LogData): String {
-        return "${logLineDataFormat.format(Date(log.timestamp))} ${log.tag.replace(" ", "-")} ${log.level.getCode()} ${log.message}\n"
+        return "${logLineDataFormat.format(Date(log.timestamp))} ${log.tag.replace(" ", "-")} ${log.level} ${log.message}\n"
     }
 }
